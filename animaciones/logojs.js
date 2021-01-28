114b647e7a366bf1a9dc50a959ff07bb7ad30332
agrishurtado <agrishurtado@gmail.com>
2021-01-28 01:47:34 -0600

    funcion completa de animacion principal

--- animaciones/logojs.js
+++ animaciones/logojs.js
@@ -1,6 +1,6 @@
 let tr=gsap.timeline();
 tr.to('#logjs',3, { 
     x: '400%', 
-    opacity: 0.7, 
+    opacity: .5, 
     ease: Power4.easeOut,
     delay:2 }, '=-0.1')
--- animaciones/logojs2.js
+++ animaciones/logojs2.js
@@ -1,6 +1,6 @@
 let tc=gsap.timeline();
 tc.to('#logjs2',3, { 
     x: '-400%', 
-    opacity: 0.7, 
+    opacity: .5, 
     ease: Power4.easeOut,   
     delay: 2}, '=-0.1')
